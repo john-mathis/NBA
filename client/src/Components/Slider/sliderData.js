75a5c010d7d80adb39c3df7a4f09bdb1b65e3fcd
John Mathis <johnmathis98@gmail.com>
2023-04-12 22:43:10 -0400

    slider edits

--- client/src/Components/Slider/sliderData.js
+++ client/src/Components/Slider/sliderData.js
@@ -15,22 +15,20 @@ export const SliderData = [
   {
     id: 2,
     header: "NUMBER ONE NUGGETS ",
-    description:
-      "'Denver's new additions may be enough to put them over the top' - Shaquille O'Neal",
+    description: "'Denver looks questionable entering the post season.",
     image: "../../assets/nuggets-duo.jpg",
   },
   {
     id: 3,
     header: "NEW FASHIONED NETS",
     description:
-      "The Brookyln Nets are looking forward to building around fresh faces in the big city. ",
+      "The Brookyln Nets are looking forward to building around fresh faces. ",
     image: "../../assets/nets-team.jpg",
   },
   {
     id: 4,
     header: "LIGHT ARMED LAKERS",
-    description:
-      "With the last few games of the season approaching, the Los Angeles Lakers have a real shot at a playoff berth.",
+    description: "Los Angeles Lakers have clenched the 7th seed.",
     image: "../../assets/lakers.jpg",
   },
 ];
