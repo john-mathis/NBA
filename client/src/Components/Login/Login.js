0c9342a86a3376e8328ee16be46475603698f2f1
John Mathis <johnmathis98@gmail.com>
2023-04-26 20:54:45 -0400

    removed console.log

--- client/src/Components/Login/Login.js
+++ client/src/Components/Login/Login.js
@@ -22,7 +22,7 @@ const Login = () => {
       setCookies("access_token", response.data.token);
       window.localStorage.setItem("userID", response.data.userID);
       window.localStorage.setItem("username", username);
-      console.log(response);
+
       navigate("/");
     } catch (err) {
       console.error(err);
